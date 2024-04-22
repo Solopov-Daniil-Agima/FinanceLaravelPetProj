aea183d7d4675ff04b55a793e042ccbb28ce4dda
Solopov Daniil <d.solopov@agima.ru>
2024-04-22 20:35:18 +0300

    Добавил подобие безопастности

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Transactions\CreateTransactionController;
 use App\Http\Controllers\Transactions\GetExcelController;
+use App\Http\Middleware\ForceHttpsMiddleware;
+use App\Http\Middleware\XssMiddleware;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware([ForceHttpsMiddleware::class, XssMiddleware::class])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 
-Route::post('/createTransaction', [CreateTransactionController::class, 'createTransaction'])->name('createTransaction');
-Route::post('/getExcelFile', [GetExcelController::class, 'getExcelFile'])->name('exportExcel');
+    Route::post('/createTransaction', [CreateTransactionController::class, 'createTransaction'])->name('createTransaction');
+    Route::post('/getExcelFile', [GetExcelController::class, 'getExcelFile'])->name('exportExcel');
+});
